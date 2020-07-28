f8136beb994947275f8ef725a9800e245a8221cb
Behzad Pouyanfar <bzad0000@gmail.com>
2020-07-28 15:51:45 -0400

    initial commit

---
+++ app/components/Panel.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+class Panel extends React.Component {
+    render() {
+        return (
+            <div className="panel panel-default">
+                <div className="panel-heading">
+                    Non eu sit duis adipisicing esse incididunt ad proident.
+                </div>
+                <div className="panel-body">
+                    Aliquip dolore commodo nostrud minim. Cillum do enim non ullamco. Commodo magna eu ex mollit sunt
+                    amet fugiat. In irure eu enim id ea sit nostrud incididunt ad adipisicing.Aliquip dolore commodo
+                    nostrud minim. Cillum do enim non ullamco. Commodo magna eu ex mollit sunt amet fugiat. In irure eu
+                    enim id ea sit nostrud incididunt ad adipisicing.
+                </div>
+            </div>
+        );
+    }
+};
+
+
+export default Panel;
